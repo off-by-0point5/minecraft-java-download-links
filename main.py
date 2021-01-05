fc8a93120cbfe975638f3ff70f3dd97d3ab9473d
offbyone <offbyone@disroot.org>
2021-01-05 22:39:20 +0100

    Make program less waiting; Adapt to new site html structure.

--- main.py
+++ main.py
@@ -24,71 +24,79 @@ def get_html(url: str):
             print('read from file')
     except FileNotFoundError:
         if online:
+            sleep_time = (last_download + timeout) - datetime.utcnow().timestamp()
+            if sleep_time > 0:
+                print(f'Sleep {sleep_time} seconds.')
+                time.sleep(sleep_time)
             html = requests.get(web_root + url).text
+            last_download = datetime.utcnow().timestamp()
             if not p.exists():
                 p.mkdir(parents=True)
             with p.joinpath('html').open('w') as f:
                 f.write(html)
                 print('downloaded')
-            sleep_time = (last_download + timeout) - datetime.utcnow().timestamp()
-            if sleep_time > 0:
-                time.sleep(sleep_time)
-            last_download = datetime.utcnow().timestamp()
         else:
             html = ''
     return html
 
 
 web_root = 'https://mcversions.net/'
-versions_json = {}
+# Map Tag keys to strings
+group_names = ['stable', 'snapshot', 'beta', 'alpha']
+group_keys = []
+# List of version html tags
+tag_list = []
+# Dict of versions
 versions_list = []
-versions_path = Path('versions.json')
-if versions_path.is_file():
-    with versions_path.open('r') as file:
-        versions_json = json.loads(file.read())
-soup = BeautifulSoup(get_html('/'), "html.parser")
-versions = soup.find('div', {'class': 'versions'})
-named_types = versions.find_all('div', recursive=False)
-c1 = 0
-for n in named_types:
-    c1 += 1
-    name = n.find('h5').get('class')[0]
-    if not versions_json.get(name):
-        versions_json[name] = {}
-    n = n.find('div', {'class': 'items'}).find_all(lambda tag: tag.name == 'div' and tag.get('class') == ['item'])
-    c2 = 0
-    for e in n:
-        c2 += 1
-        dl_link_site = e.find('a', {'class': 'button'}).get('href')
-        item = e.get('id')
-        release_time = e.find('time').text
-        print(str(c1)+'/'+str(len(named_types))+' | '+str(c2)+'/'+str(len(n))+' | ', end='')
-        list_item = {
-            'date': release_time,
-            'type': name,
-            'version': item,
-        }
-        if not versions_json[name].get(item):
-            versions_json[name][item] = {}
-            dl_soup = BeautifulSoup(get_html(dl_link_site), "html.parser")
-            downloads = dl_soup.find_all('div', {'class': 'download'})
-            for d in downloads:
-                if d.find('h5'):
-                    side = d.find('h5').text.split(' ')[0].lower()
-                    dl_link = d.find('a', {'class': 'button'}).get('href')
-                    versions_json[name][item][side] = dl_link
-                    list_item[side] = dl_link
-        else:
-            print('indexed')
-            for side in versions_json[name][item]:
-                if side in ['server', 'client']:
-                    list_item[side] = versions_json[name][item][side]
-        versions_json[name][item]['date'] = release_time
-        versions_list.append(list_item)
+versions_tree = {}
 
-with open('versions.json', 'w') as file:
-    # print(json.dumps(versions_json, indent=2))
-    file.write(json.dumps(versions_json, indent=2))
+with open('versions-list.json', 'rt') as file:
+    versions_list = json.loads(file.read())
+
+soup = BeautifulSoup(get_html('/'), "html.parser")
+all_divs = soup.find_all('div')
+for div in all_divs.copy():
+    if div.get('id') is None or div.get('data-version') is None:
+        all_divs.remove(div)
+
+for d in all_divs:
+    if d.parent not in group_keys:
+        group_keys.append(d.parent)
+        versions_tree[group_names[group_keys.index(d.parent)]] = []
+    tag_list.append(d)
+    versions_tree[group_names[group_keys.index(d.parent)]].append(tag_list.index(d))
+for version in tag_list:
+    # print(version)  # print html tag found for version
+    download_link = version.find('a').get('href')
+    version = {'date': version.find('time').text,
+               'type': group_names[group_keys.index(version.parent)],
+               'version': version.get('data-version')}
+    for indexed_version in versions_list:
+        if version['version'] == indexed_version['version']:
+            print(f"{version['version']} already indexed.")
+            break
+    else:
+        download_soup = BeautifulSoup(get_html(download_link), "html.parser")
+        binaries = download_soup.find_all('a')
+        binary_locations = {}
+        for b in binaries:
+            if b.get('href').endswith('client.jar'):
+                binary_locations['client'] = b.get('href')
+            if b.get('href').endswith('server.jar'):
+                binary_locations['server'] = b.get('href')
+        version.update(binary_locations)
+        versions_list.append(version)
+        # print(version)  # print versin json
+
+
+versions_tree = {}
+for version_dict in versions_list:
+    if version_dict['type'] not in versions_tree:
+        versions_tree[version_dict['type']] = {}
+    versions_tree[version_dict['type']][version_dict['version']] = version_dict
+
+with open('versions-tree.json', 'w') as file:
+    file.write(json.dumps(versions_tree, indent=2))
 with open('versions-list.json', 'w') as file:
     versions_list = sorted(versions_list, key=lambda k: k['date'], reverse=True)
     file.write(json.dumps(versions_list, indent=2))
